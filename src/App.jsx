a77db3563e881180953f00efd62d39f15b6d3574
Nasweha <nasweha@gmail.com>
2024-04-28 10:44:45 +0530

    #Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import { useState } from 'react'
-
 import './App.css'
 import Hospital from './pages/Hospital'
-import Home from './pages/Home'
 import Form from './pages/Form'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+
+
 
 function App() {
 
 
   return (
     <>
-      <Hospital/>
-      <Home/>
+      
+
       {/* <Hospital/> */}
-      <Form/>
+      
+      <Routes>
+        <Route path='/home' element={<Home/>} />
+        <Route path='/' element={<Hospital/>} />
+        <Route path='/form' element={<Form/>} />
+
+      </Routes>
+
+
+
+
+
     </>
   )
 }
