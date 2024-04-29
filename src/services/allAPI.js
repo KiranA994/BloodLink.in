dc20c1c3ad989742223f41eadbe5510e7156be3e
KiranA994 <kiranagrasala13@gmail.com>
2024-04-29 07:55:52 +0530

    api for login and delete a user added

--- src/services/allAPI.js
+++ src/services/allAPI.js
@@ -7,3 +7,15 @@ import { serverUrl } from './baseUrl'
 export const registerUserApi = async(reqBody) => {
     return await commonAPI('POST',`${serverUrl}/register`,reqBody)
 }
+
+// api for login
+
+export const loginUserApi = async(reqBody) => {
+    return await commonAPI('POST', `${serverUrl}/login`, reqBody)
+}
+
+// api to delete a post of a particular user
+
+export const deleteUserApi = async(id) => {
+    return await commonAPI('DELETE', `${serverUrl}/deleteUser/${id}`,{})
+}
