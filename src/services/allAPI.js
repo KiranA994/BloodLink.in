d876967d27c1cc971638edf9391dab9dd85e7ca9
KiranA994 <kiranagrasala13@gmail.com>
2024-04-29 11:04:29 +0530

    api to get all data is created

--- src/services/allAPI.js
+++ src/services/allAPI.js
@@ -1,21 +1,27 @@
-import {commonAPI} from './commonAPI'
+import { commonAPI } from './commonAPI'
 import { serverUrl } from './baseUrl'
 
 
 //  api for registering user
 
-export const registerUserApi = async(reqBody) => {
-    return await commonAPI('POST',`${serverUrl}/register`,reqBody)
+export const registerUserApi = async (reqBody) => {
+    return await commonAPI('POST', `${serverUrl}/register`, reqBody)
 }
 
 // api for login
 
-export const loginUserApi = async(reqBody) => {
+export const loginUserApi = async (reqBody) => {
     return await commonAPI('POST', `${serverUrl}/login`, reqBody)
+}
+
+// api to get all data
+
+export const getAllUserApi = async() => {
+    return await commonAPI('GET', `${serverUrl}/bloodInfo`,'')
 }
 
 // api to delete a post of a particular user
 
-export const deleteUserApi = async(id) => {
-    return await commonAPI('DELETE', `${serverUrl}/deleteUser/${id}`,{})
+export const deleteUserApi = async (id) => {
+    return await commonAPI('DELETE', `${serverUrl}/deleteUser/${id}`, {})
 }
