253c579015b9b30d11e9a4029a803e1c6a1e55e0
Najas06 <najasneju@gmail.com>
2024-05-02 00:11:53 +0530

    form navigate

--- src/components/Enquire.jsx
+++ src/components/Enquire.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Enquire = () => {
 
-  // navigate next page with a delay 
-  const navigate = useNavigate('')
+//   // navigate next page with a delay 
+//   const navigate = useNavigate('')
 
 
- const navigateToForm = () => {
-    setTimeout(() => {
-        navigate('/form')
-      }, 3000)
- }
+//  const navigateToForm = () => {
+//     setTimeout(() => {
+//         navigate('/form')
+//       }, 3000)
+//  }
   return (
     <>
+    <Link to={'/form'}>
     <button style={{ border: "none", backgroundColor: "red", color: "white"  }} onClick={navigateToForm} className='p-2 border border-2 rounded'>Connect to Blood Bank</button>
+    </Link>
     </>
   )
 }
